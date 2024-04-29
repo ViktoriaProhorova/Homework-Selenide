a62b9e6ad431ecaa4116fb3a480ac5dac24fe7ac
ViktoriaProhorova <viktoriyaprokhorova7@gmail.com>
2024-04-29 18:04:18 +0200

    Commit for Selenide homework

---
+++ src/test/java/TestCart.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import pages.CartPage;
+import pages.HomePage;
+import pages.ProductPage;
+
+
+public class TestCart extends TestBase {
+
+    @Test
+    public void removeFromCartTest() {
+        String expectedEmptyCartMessage = "There are no items in your cart.";
+
+        HomePage homePage = page(HomePage.class);
+        ProductPage productPage = page(ProductPage.class);
+        CartPage cartPage = page(CartPage.class);
+
+        homePage.clickRedDuckFromMostPopularSection();
+        productPage.clickAddToCartButton();
+        cartPage.waitingUntilProductIsAddedToCart();
+        productPage.clickCheckoutButton();
+
+        cartPage.clickRemoveFromCartButton();
+        cartPage.waitingUntilBackButtonAppearsOnThePage();
+
+        cartPage.getEmptyCartMessageElement().shouldHave(text(expectedEmptyCartMessage));
+    }
+
+    @Test
+    public void checkDuckPriceIsCorrect() {
+        HomePage homePage = page(HomePage.class);
+        ProductPage productPage = page(ProductPage.class);
+        CartPage cartPage = page(CartPage.class);
+
+        homePage.clickRedDuckFromMostPopularSection();
+
+        productPage.clickAddToCartButton();
+        cartPage.waitingUntilProductIsAddedToCart();
+        productPage.clickCheckoutButton();
+
+        cartPage.getProductPriceElement().shouldHave(text("$20.00"));
+    }
+}
+
